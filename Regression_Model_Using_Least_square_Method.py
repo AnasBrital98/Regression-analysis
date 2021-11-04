a3e7d1e9dcd8e178d4bf1f3f43d029915aeb0895
Anas Brital <anass.brital98@gmail.com>
2021-11-04 13:24:31 +0100

    Adding Linear Regression Using Least Square Method

---
+++ Regression_Model_Using_Least_square_Method.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_regression
+
+x , y = make_regression(n_samples=100 , n_features= 1 , noise=10)
+
+
+"""
+Our Linear Model Looks like this :   Y_estimated = theta[0] + theta[1] * x .
+our goal is to compute the coefficients Theta[0] and Theta[1] that will fit our data correctly,
+Using Least Square Method . 
+
+"""
+# Calculating The Mean for x and y
+X_mean = np.mean(x)
+Y_mean = np.mean(y)
+
+# Calculating The Coefficient Theta_1
+num = np.sum( (x[i] - X_mean)*(y[i] - Y_mean) for i in range(len(x)) )
+den = np.sum( (x[i] - X_mean)**2 for i in range(len(x)) )
+theta_1 = num / den
+
+# Calculating The Coefficient Theta_0
+theta_0 = Y_mean - theta_1 * X_mean
+
+# Calculating The Estimated Y
+Y_estimated = theta_0 + theta_1 * x
+
+# Plot The Result
+
+plt.figure()
+plt.scatter(x , y , color='blue')
+plt.plot(x , Y_estimated ,label = 'Estimated Value' )
+plt.xlabel('Independent Variable')
+plt.xlabel('dependent Variable')
+plt.title('Linear Regression Using Least Square Method')
+plt.legend(loc = 'lower right')
+plt.show()
