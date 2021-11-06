Fixing Polynomial Regression with Gradient Descent and Adding some resources

Polynomial_Regression_Using_Gradient_Descent.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import make_regression
 
 def Generate_Points(start , end , nbr_points , coefficient , noise ):
     
@@ -41,17 +42,27 @@ class Polynomial_Reression :
     
     def compute_cost(self , X , theta):
         hypothesis = self.compute_hypothesis(X, theta)
-        cost = (1/len(self.y)) * np.sum((self.y - hypothesis) ** 2 )
+        n_samples = len(self.y)
+        error = hypothesis - self.y
+        cost = (1/2 * n_samples) * np.sum((error ) ** 2 )
         return cost
     
-    def fit(self , order = 2 , epsilon = 10e-3 , nbr_iterations = 50 , learning_rate = 10e-3):
+    def Standardize_Data(self ,x):
+        return (x - np.mean(x)) / (np.max(x) - np.min(x))
+    
+    def fit(self , order = 2 , epsilon = 10e-3 , nbr_iterations = 1000 , learning_rate = 10e-1):
         
         self.order = order
         self.nbr_iterations = nbr_iterations
         
-        X = [self.x ** i for i in range(order+1)]
+        #X = [self.x ** i for i in range(order+1)]
+        X = []
+        X.append(np.ones(len(self.x)))
+        for i in range(1 , order + 1):
+            X.append(self.Standardize_Data(self.x ** i))
+        
         X = np.column_stack(X)
-        theta = np.random.randn(order+1 , 1)
+        theta = np.random.randn(order+1)
         costs = []
         
         for i in range(self.nbr_iterations):
@@ -60,21 +71,22 @@ class Polynomial_Reression :
             hypothesis = self.compute_hypothesis(X, theta)
             
             # Computing The Errors
-            errors =  self.y - hypothesis
+            errors =  hypothesis - self.y
             
             # Update Theta Using Gradient Descent 
             n_samples = len(self.y)
-            d_J = np.dot(X.T , errors)
-            theta -= learning_rate * (1/n_samples) * np.sum(d_J , axis = 1)
+            d_J = (1/ n_samples) * np.dot(X.T , errors)
+            theta -= learning_rate *  d_J 
             
             # Computing The Cost
             cost = self.compute_cost(X, theta)
             costs.append(cost)            
             
             # if the current cost less than epsilon stop the gradient Descent
+            
             if cost < epsilon :
                 break
-        
+            
         self.costs = costs
         self.X = X
         self.theta = theta    
@@ -82,11 +94,30 @@ class Polynomial_Reression :
     def plot_line(self):
         plt.figure()
         plt.scatter(self.x , self.y , color = 'blue')
+        
+        # Line for Order 1
         Y_hat = self.compute_hypothesis(self.X , self.theta)
-        plt.plot(self.x , Y_hat , "-r")
+        plt.plot(self.x , Y_hat , "-r" , label = 'Order = ' + str(self.order) )
+        
+        # Line for Order 2
+        self.fit(order = 2)
+        Y_hat = self.compute_hypothesis(self.X , self.theta)
+        plt.plot(self.x , Y_hat , "-g" , label = 'Order = ' + str(self.order) )
+        
+        # Line for Order 3
+        self.fit(order = 3)
+        Y_hat = self.compute_hypothesis(self.X , self.theta)
+        plt.plot(self.x , Y_hat , "-m" , label = 'Order = ' + str(self.order) )
+        
+        # Line for Order 4
+        self.fit(order = 4)
+        Y_hat = self.compute_hypothesis(self.X , self.theta)
+        plt.plot(self.x , Y_hat , "-y" , label = 'Order = ' + str(self.order) )
+        
         plt.xlabel("independent variable")
         plt.ylabel("dependent variable")
         plt.title("Polynomial Regression Using Gradient Descent")
+        plt.legend(loc = 'lower right')
         plt.show()
         
     def plot_cost(self):
@@ -98,9 +129,8 @@ class Polynomial_Reression :
         plt.legend(loc = 'lower right')
         
 if __name__ == "__main__":
-    
-    x,y = Generate_Points(0, 10, 100, [3,2,1], 10)
+    x,y = Generate_Points(0, 50, 100, [3, 1, 1], 2.3)
     Poly_regression = Polynomial_Reression(x, y)
-    Poly_regression.fit(order = 2)
+    Poly_regression.fit(order = 1)
     Poly_regression.plot_line()
     Poly_regression.plot_cost()